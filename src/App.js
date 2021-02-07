8576e0caf362f5d4dd1813c7565cb64917da0240
Rajgokul80565 <74341639+Rajgokul80565@users.noreply.github.com>
2021-02-07 11:41:49 +0530

    Fetching data from api and rendered dynamically(implemented search )

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import {useEffect,useState} from 'react';
 import Card from './components/Cards';
@@ -16,13 +16,28 @@ const appId = '0735d9a3';
 const appKey = 'b9711dc39155ee71072a0465451ddf60';
 
 //
-const url =`https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}`;
 
 const [recipes, setRecipes] = useState([]);
+const [search,setSearch] = useState("");
+const [query,setQuery] = useState('chicken');
+
 
 useEffect(() =>{
   getrecipes();
-},[])
+},[query]);
+
+const updateSearch = e =>{
+  setSearch(e.target.value);
+}
+
+const getSearch = e =>{
+  e.preventDefault();
+  setQuery(search);
+  setSearch('');
+}
+
+const url =`https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`;
+
 
 
 
@@ -39,21 +54,24 @@ const getrecipes = async () =>{
 
   return (
     <div className="App">
-     <form >
+     <form onSubmit = {getSearch} >
        <h2>Enter you Keywords</h2>
-       <input type="text"/>
+       <input
+        type="text"
+         value={search}
+         onChange = {updateSearch} 
+        />
        <button  type='submit' >search</button>
      </form>
      {recipes.map(recipe => (
         <Card 
+          key={recipe.recipe.label}
           title = {recipe.recipe.label}
-          steps = {recipe.recipe.ingredientLines}
+          ingredients = {recipe.recipe.ingredients}
           calories = {recipe.recipe.calories}
           images = {recipe.recipe.image}
         />
      ))}
-
-     <Card />
      
     </div>
   );
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,18 +1,24 @@
+import React from 'react';
 
 
-
-function Card({title,steps,calories,images}){
+function Card({title,ingredients,calories,images}){
     return(
 
         <div className = 'Card'>
             <h1 className = 'title'>{title}</h1>
             <img src={images} alt=""/>
-            <p className = 'recipes'>{steps}</p>
+            {ingredients.map( ingre=>(
+              <h6>{ingre.text}</h6>
+             ))}
+          
             <h5 className = 'calories'>{calories}</h5>
         </div>
 
 
-    )
-}
+    );
+};
 
 export default Card;
+
+//You may want to add 'directions' to the recipes, so the users know how to make them. If so in App.js when he maps through recipe just add a 'directions= {recipe.recipe.url}' , then in Recipe.js add '<a href={directions}
+  //target='_blank'>  Directions  </a>'  Hope that helps someone. Awesome video! Thanks so much! I had a lot of fun with it!
